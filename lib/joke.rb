9ab8d2967fd8d8d892a0c04942c5a142479e7332
Marshall Houston <marshall.houston@gmail.com>
2017-09-06 09:13:38 -0600

    iteration 1 complete

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,29 @@
 class Joke
+attr_reader :joke_information
+
+  def initialize(joke_information)
+    @joke_information = joke_information
+  end
+
+  def id
+    id_info = @joke_information.find do |info|
+      info[1] == 1
+    end
+    id_info[1]
+  end
+
+  def question
+    question_info = @joke_information.find do |info|
+      info[1] == "Why did the strawberry cross the road?"
+    end
+    question_info[1]
+  end
+
+  def answer
+    answer_info = @joke_information.find do |info|
+      info[1] == "Because his mother was in a jam."
+    end
+    answer_info[1]
+  end
 
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -5,4 +5,31 @@ require './lib/joke'
 
 class JokeTest < Minitest::Test
 
+  def test_it_exists
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_instance_of Joke, joke
+  end
+
+  def test_it_can_return_its_id
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    assert_equal 1, joke.id
+  end
+
+  def test_it_can_return_its_question
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    expected = "Why did the strawberry cross the road?"
+    actual = joke.question
+    assert_equal expected, actual
+  end
+
+  def test_it_can_return_its_answer
+    joke = Joke.new({id: 1, question: "Why did the strawberry cross the road?", answer: "Because his mother was in a jam."})
+
+    expected = "Because his mother was in a jam."
+    actual = joke.answer
+    assert_equal expected, actual
+  end
 end
